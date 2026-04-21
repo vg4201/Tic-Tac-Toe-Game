d807a2835a092df9ce9752f1e9fed4b4c7746db5
vyshnavi <vg4201@srmist.edu.in>
2026-04-21 14:54:36 +0530

    Implemented UC1 display board

---
+++ src/main/java/TicTacToe.java
@@ -0,0 +1,17 @@
+public class TicTacToe {
+    public static void main(String[] args) {
+        char[][] board = new char[3][3];
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[i].length; j++) {
+                board[i][j] = '-';
+            }
+        }
+        System.out.println("Empty Tic-Tac-Toe Board:");
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[i].length; j++) {
+                System.out.print(board[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
